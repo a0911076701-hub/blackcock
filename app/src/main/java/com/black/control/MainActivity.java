56d5849f93284dd6fdf42593d19f004ed6eb610d
a0911076701-hub <a0911076701@gmail.com>
2026-07-13 21:23:16 +0200

    Full control panel: Firebase integration, bottom navigation, all commands, file viewer, screen streaming, settings

--- app/src/main/java/com/black/control/MainActivity.java
+++ app/src/main/java/com/black/control/MainActivity.java
@@ -6,16 +6,15 @@ import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView webView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        webView = findViewById(R.id.webView);
+        WebView webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(true);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/control_panel.html");
     }
